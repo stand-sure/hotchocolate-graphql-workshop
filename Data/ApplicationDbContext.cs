93e7d84811e6d4c72b7fd73ba3567de18fe27dfc
C. J. Anderson <chris@standsure.io>
2023-02-02 15:12:24 -0500

    Added DbContext and initial migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,22 @@
 namespace ConferencePlanner.Data;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 public class ApplicationDbContext : DbContext
 {
+    private IConfiguration Configuration { get; }
+
     /// <summary>
     /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class.
     /// </summary>
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
     {
+        this.Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(this.Configuration.GetConnectionString("ConferencePlanner"));
     }
 }
--- Service/Program.cs
+++ Service/Program.cs
@@ -1,5 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
+using ConferencePlanner.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddJsonConsole();
+
+builder.Configuration.AddJsonFile("appsettings.json");
+
+builder.Services.AddDbContext<ApplicationDbContext>((provider, optionsBuilder) =>
+{
+    optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("ConferencePlanner"), contextOptionsBuilder => contextOptionsBuilder.MigrationsAssembly("Service"));
+});
+
+WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
