4f73e0232d1e308a1c581f48848cce0ea5d55e23
C. J. Anderson <chris@standsure.io>
2023-02-02 16:32:35 -0500

    added Speakers

--- Service/Migrations/20230202213112_Speakers.Designer.cs
+++ Service/Migrations/20230202213112_Speakers.Designer.cs
@@ -2,6 +2,7 @@
 using ConferencePlanner.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ConferencePlanner.Service.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230202213112_Speakers")]
+    partial class Speakers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
